ac9945dc460f3b72256dc86cc2da5a84701e5e2c
HarriHiie <89829583+HarriHiie@users.noreply.github.com>
2023-02-14 08:59:06 +0200

    Add files via upload

---
+++ var.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var domain = new Dictionary<string, string>()
+            {
+                {"est", "Estonia"},
+                {"lat", "Latvia"},
+                {"fin", "Finland"},
+                {"swe", "Sweden"}
+            };
+
+            domain.ToList().ForEach(x => Console.WriteLine($"{x.Key} - {x.Value}"));
+        }
+    }
+}
